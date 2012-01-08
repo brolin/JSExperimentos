d2bc8cbd4208bcc64fe1a9322fdeba3ded0c6591
brolin <brolin108@gmail.com>
2012-01-08 02:35:37 -0500

    muestra lista de evento, por ahora solo años

--- timeline/public/javascripts/client.js
+++ timeline/public/javascripts/client.js
@@ -6,12 +6,19 @@ $.widget("ui.Timeline", {
 	$.get('/timelines', function(data) {
 	    var count= 0;
 	    data.forEach(function(t) {
+		// console.log(t.anio);
 		count++;
 		var $layout= $(".timeline-item.layout").clone();
 		$layout.removeClass("layout");
-		$layout.find(".name").text(t.name);
+		$layout.find(".name").text(t.nombre);
+		// Tengo que poner una serie de elementos <p> que correspondan con cada elemento de los antecedentes t.anio???</p>
 		$layout.addClass("item-"+count);
 		$(".timelines-list ul").append($layout.show());
+		t.anio.forEach(function(y) { //Add todos los <p>s par, solo está poniendo el último
+		    var $evento= $("li.timeline-item.item-"+count);
+		    // console.log($evento.find(".event"));
+		    $evento.append("<p class=\"event\">"+y+"</p>");
+		});
 	    });
 	});
     }
